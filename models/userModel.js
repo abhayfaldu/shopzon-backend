e584e74bb47731dbd483a12d65314195854f5c3b
abhayfaldu <abhayfaldu1922@gmail.com>
2023-08-08 10:05:21 +0530

    added mobile in the user schema

--- models/userModel.js
+++ models/userModel.js
@@ -7,6 +7,7 @@ const userSchema = new mongoose.Schema(
 		firstName: { type: String, required: true },
 		lastName: { type: String, required: true },
 		email: { type: String, required: true, unique: true },
+		mobile: { type: String, required: true, unique: true },
 		password: { type: String, required: true },
 		role: { type: String, default: "user" },
 	},
