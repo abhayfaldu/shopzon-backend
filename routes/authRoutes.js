b5f15c768e72cfea9fb543d26fe62a7443829aa7
abhayfaldu <abhayfaldu1922@gmail.com>
2023-09-23 08:53:48 +0530

    added more features about user authentication

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const router = express.Router();
 const {
 	createUser,
 	loginUserController,
@@ -7,13 +6,20 @@ const {
 	getAUser,
 	updateUser,
 	deleteUser,
+	blockUser,
+	unblockUser,
 } = require("../controllers/userController.js");
+const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware.js");
+
+const router = express.Router();
 
 router.post("/register", createUser);
 router.post("/login", loginUserController);
 router.get("/all-users", getAllUsers);
-router.get("/:id", getAUser);
-router.patch("/:id", updateUser);
+router.get("/:id", authMiddleware, isAdmin, getAUser);
 router.delete("/:id", deleteUser);
+router.put("/edit-user", authMiddleware, updateUser);
+router.put("/block-user/:id", authMiddleware, isAdmin, blockUser);
+router.put("/unblock-user/:id", authMiddleware, isAdmin, unblockUser);
 
 module.exports = router;
