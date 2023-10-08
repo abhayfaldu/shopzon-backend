1b75f63f54b3d26de26350914e293500c4df77dc
abhayfaldu <abhayfaldu1922@gmail.com>
2023-10-08 07:49:39 +0530

    implemented pagination in get all products

--- controllers/productController.js
+++ controllers/productController.js
@@ -43,6 +43,18 @@ const getAllProducts = asyncHandler(async (req, res) => {
 			query = query.select("-__v");
 		}
 
+		// pagination
+		const page = req.query.page;
+		const limit = req.query.limit;
+		const skip = (page - 1) * limit;
+		query = query.skip(skip).limit(limit);
+		if (req.query.page) {
+			const productCount = await Product.countDocuments();
+			if (skip >= productCount) {
+				throw new Error("This page does not exists");
+			}
+		}
+
 		const products = await query;
 		res.json(products);
 	} catch (error) {
