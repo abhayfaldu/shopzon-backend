8120cf0b4dfbfdaf79ebe51e04aba1268e2d7fd1
abhayfaldu <abhayfaldu1922@gmail.com>
2023-10-14 10:27:47 +0530

    added getAllCategories and deleteCategory route and controller

--- routes/productCategoryRoute.js
+++ routes/productCategoryRoute.js
@@ -1,9 +1,16 @@
 const express = require("express");
-const { createCategory, updateCategory } = require("../controllers/productCategoryControllers");
+const {
+	createCategory,
+	updateCategory,
+	getAllCategories,
+	deleteCategory,
+} = require("../controllers/productCategoryControllers");
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
+router.get("/", getAllCategories);
 router.post("/", authMiddleware, isAdmin, createCategory);
 router.put("/:id", authMiddleware, isAdmin, updateCategory);
+router.delete("/:id", authMiddleware, isAdmin, deleteCategory);
 
 module.exports = router;
