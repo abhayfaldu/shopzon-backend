9001ba233738c54d17a02f70273fde331e9e615d
abhayfaldu <abhayfaldu1922@gmail.com>
2023-10-16 21:18:14 +0530

    implemented get all coupon

--- routes/couponRoute.js
+++ routes/couponRoute.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const {
 	createCoupon,
+	getAllCoupons,
 } = require("../controllers/couponController");
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 router.post("/", authMiddleware, isAdmin, createCoupon);
+router.get("/", authMiddleware, isAdmin, getAllCoupons);
 
 module.exports = router;
