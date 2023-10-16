2ca17981a4f4551a456fb150a60afb9fc400de17
abhayfaldu <abhayfaldu1922@gmail.com>
2023-10-16 22:22:10 +0530

    implemented update and delete coupon

--- controllers/couponController.js
+++ controllers/couponController.js
@@ -1,4 +1,5 @@
 const Coupon = require("../models/couponModel");
+const validateMongodbId = require("../utils/validateMongodbId");
 const asyncHandler = require("express-async-handler");
 
 const createCoupon = asyncHandler(async (req, res) => {
@@ -19,4 +20,30 @@ const getAllCoupons = asyncHandler(async (req, res) => {
 	}
 });
 
-module.exports = { createCoupon, getAllCoupons };
+const updateCoupon = asyncHandler(async (req, res) => {
+	const { id } = req.params;
+	validateMongodbId(id);
+
+	try {
+		const updatedCoupon = await Coupon.findByIdAndUpdate(id, req.body, {
+			new: true,
+		});
+		res.json(updatedCoupon);
+	} catch (error) {
+		throw new Error(error);
+	}
+});
+
+const deleteCoupon = asyncHandler(async (req, res) => {
+	const { id } = req.params;
+	validateMongodbId(id);
+
+	try {
+		const deletedCoupon = await Coupon.findByIdAndDelete(id);
+		res.json(deletedCoupon);
+	} catch (error) {
+		throw new Error(error);
+	}
+});
+
+module.exports = { createCoupon, getAllCoupons, updateCoupon, deleteCoupon };
